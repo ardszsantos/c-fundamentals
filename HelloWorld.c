6654e53b459c13afc01a57d93b5b6be72b3a8075
ardszsantos <lokgamer01@gmail.com>
2024-10-21 16:50:25 -0300

    started

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("Hello World!\n");
+    //printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+    printf("\\I like pizza\\ - Abraham Lincoln Probably");
+    return 0;
+}
