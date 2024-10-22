e161312d34b71af031f44bc983fdc6e134563391
ardszsantos <lokgamer01@gmail.com>
2024-10-22 13:47:13 -0300

    data types

---
+++ dataTypes/data-types.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main() {
+
+
+    float c = 3.141592;
+    double b = 3.141592653589793;
+
+    bool e = true;
+
+    char f = 120;
+    unsigned char g = 255;
+
+    short int h = 32767;
+    unsigned short int i = 65535;
+
+    int j = 2147483647;
+    unsigned int k = 4294967295;
+
+
+    long long int l = 9223372036854775807;
+    unsigned long long int m = 18446744073709551615;
+
+
+    return 0;
+}
