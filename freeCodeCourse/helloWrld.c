be1680d0d5c0bed7f036d11ae17a62f4b99ce49e
ardszsantos <lokgamer01@gmail.com>
2024-11-02 17:26:50 -0300

    test

---
+++ freeCodeCourse/helloWrld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int top() {
+
+    char name[] = "peruzones";
+    int age = 45;
+
+    printf("Hello my name is %s and i have %d years old", name, age);
+    return 0;
+}
