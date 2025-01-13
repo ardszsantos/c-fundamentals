15bc15180014bd78ed3d9176e793d80ae621a0ef
ardszsantos <lokgamer01@gmail.com>
2025-01-13 14:42:27 -0300

    added a break and a loop

--- alura/guessGame.c
+++ alura/guessGame.c
@@ -11,29 +11,28 @@ int main() {
 
     int chute;
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d \n", chute);
+    for(int i = 1; i <= 3; 1) {
+        printf("Tentativa %d\n", i);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d \n", chute);
 
-    int acertou = (chute == numerosecreto);
-
-
-    if(acertou) {
-        printf("Parabéns! Você acertou!\n");
-        printf("Jogue de novo, você é um bom jogador!\n");
-    } else {
-
+        int acertou = (chute == numerosecreto);
         int maior = chute > numerosecreto;
 
-        if (maior) {
+        if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+            break;
+        } else if(maior) {
             printf("Seu chute foi maior que o número secreto!\n");
         } else {
             printf("Seu chute foi menor que o número secreto!\n");
         }
 
-        printf("Que pena, você errou!\n");
-        printf("Tente de novo!\n");
+        i++;
     }
+
 
     return 0;
 }
