0e674088792739c7d9d85ea4d93b5d518cd8e2cd
ardszsantos <lokgamer01@gmail.com>
2025-03-11 15:13:37 -0300

    using swithch to choose difficulty

--- alura/guessGame.c
+++ alura/guessGame.c
@@ -14,16 +14,39 @@ int main() {
     int n1 = rand(); 
 
     int numerosecreto = n1 % 100;
-    
+    int tentativas = 1;
     int chute;
 
-    
-
-    int tentativas = 1;
 
     double pontos = 1000;
 
-    while(1) {
+    int acertou = 0;
+
+    int nivel;
+    printf("Qual o nível da dificuldade? \n");
+    printf("(1) Fácil (2) Médio (3) Difícil \n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+    
+
+
+
+    int numerodetentativas;
+    
+    switch(nivel) {
+        case 1:
+            numerodetentativas = 20;
+            break;
+        case 2:
+            numerodetentativas = 15;
+            break;
+        default: 
+            numerodetentativas = 6;
+            break;
+    }
+
+
+    for(int i = 1; i <= numerodetentativas; i++){
         printf("Tentativa %d\n", tentativas);
         printf("Qual é o seu chute? ");
         scanf("%d", &chute);
@@ -39,8 +62,7 @@ int main() {
         }
 
         else if(acertou) {
-            printf("Parabéns! Você acertou!\n");
-            printf("Jogue de novo, você é um bom jogador!\n");
+
             
             break;
         } else if(maior) {
@@ -57,10 +79,17 @@ int main() {
         pontos = pontos - pontosperdidos;
     }
 
+
     printf("Fim de jogo!\n");
-    printf("Você acertou em %d tentativas! \n", tentativas);
-    printf("Pontuação: %.1f \n", pontos);
-    
+
+    if(acertou) {
+        printf("Você ganhou! \n");
+        printf("Você acertou em %d tentativas! \n", tentativas);
+        printf("Pontuação: %.1f \n", pontos);
+    } else {
+        printf("Você perdeu! Tente de novo \n"); 
+    }
+
 
 
 
