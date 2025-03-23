c8285d3cce98b9cf01c59992d4564d4a850febc7
ardszsantos <lokgamer01@gmail.com>
2025-03-23 15:43:43 -0300

    .

--- alura/course-2/forca.c
+++ alura/course-2/forca.c
@@ -17,20 +17,18 @@ int main() {
             int achou = 0;
 
             for(int j = 0; j < tentativas; j++) {
+
                 if (chutes[j] == palavrasecreta[i]) {
-                    printf("%c ");
                     achou = 1;
                     break;
                 }
             }
 
             if(achou) { 
-                printf("%c", palavrasecreta[i]);
+                printf("%c ", palavrasecreta[i]);
             } else {
                 printf("_ ");
             }
-
-            printf("_ ");
         }
         printf("\n");
 
